6a1e7b08e547198be82885b72f044e3dfc22dfa7
wml <18981745898@163.com>
2021-07-16 11:09:52 +0800

    opencv 训练模型

---
+++ lib_vd/src/main/cpp/opencv/render/FaceCheck.cpp
@@ -0,0 +1,5 @@
+#include "FaceCheck.h"
+
+extern "C" {
+
+}
---
+++ lib_vd/src/main/cpp/opencv/render/FaceCheck.h
@@ -0,0 +1,11 @@
+#ifndef VDMAKE_FACECHECK_H
+#define VDMAKE_FACECHECK_H
+
+#include "opencv2/opencv.hpp"
+
+class FaceCheck {
+
+};
+
+
+#endif //VDMAKE_FACECHECK_H
